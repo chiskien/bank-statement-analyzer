61f2b414a2e18067778dbe2a4b6fa5025b98caab
ChisKien <chiskien03214@gmail.com>
2023-02-22 22:34:46 +0700

    Create BankStatement model class

--- src/main/java/org/chiskien/BankStatement.java
+++ src/main/java/org/chiskien/BankStatement.java
@@ -1,4 +1,29 @@
 package org.chiskien;
 
+import java.time.LocalDate;
+
 public class BankStatement {
+    private final double amount;
+    private final String description;
+    private final LocalDate date;
+
+    public BankStatement(double amount, String description, LocalDate date) {
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+
 }
